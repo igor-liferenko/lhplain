88f11f67592590472444d0d849cb72da01eda825
Igor Liferenko <igor.liferenko@gmail.com>
2022-12-10 15:04:53 +0700

    edit

--- gen.sh
+++ gen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 function gen() {
   for i in "${@:2:$#}"; do
-    mftrace --formats=pfb --encoding=enc/$1 --magnification=8000 $i || echo "*********** $i FAILED *************"
+    mftrace --formats=pfb --encoding=enc/$1 --magnification=1000 $i || echo "*********** $i FAILED *************"
   done
 }
 gen lhA.enc \
@@ -24,7 +24,6 @@ gen lhA.enc \
   omr9 \
   omsl10 \
   omsl12 \
-  omsl6 \
   omsl8 \
   omsl9 \
   omss10 \
@@ -33,35 +32,25 @@ gen lhA.enc \
   omss8 \
   omss9 \
   omssbx10 \
-  omssbxo10 \
   omssdc10 \
   omssi10 \
   omssi12 \
-  omssi17 \
   omssi8 \
   omssi9 \
   omssq8 \
-  omssqi8 \
-  omvtt10
+  omssqi8
 gen lhB.enc \
   omtex8 \
   omtex9 \
   omtex10
 gen lhC.enc \
   omsltt10 \
-  omsltt9 \
-  omtcsc10 \
   omtt10 \
   omtt12 \
   omtt8 \
   omtt9
 gen lhD.enc \
   ombxti10 \
-  ombxti12 \
-  ombxti7 \
-  omff10 \
-  omfi10 \
-  omfib8 \
   omti10 \
   omti12 \
   omti7 \
@@ -70,10 +59,6 @@ gen lhD.enc \
   omu10
 gen lhE.enc \
   omcsc10 \
-  omcsc8 \
-  omcsc9 \
   omr5
 gen lhF.enc \
-  omitt10 \
-  omitt12 \
-  omitt9
+  omitt10

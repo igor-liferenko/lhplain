c3532b9e8ed50660a5b804fe6f3682dafcfda5f5
Igor Liferenko <igor.liferenko@gmail.com>
2023-02-16 20:12:37 +0700

    edit

--- gen.sh
+++ gen.sh
@@ -4,18 +4,17 @@
 # If you need to copy/paste from pdf document, run the following command on the copied text and copy it again:
 #   sed y/‘’−/\`\'-/
 
-# TODO: add missing fonts here and to omfonts.map
-
 rm -r pfb
 mkdir pfb
 function gen() {
   for i in "${@:2:$#}"; do
-    mftrace --formats=pfb --encoding=enc/$1 --magnification=1000 $i || echo "*********** $i FAILED *************"
+    mftrace --formats=pfb --encoding=enc/$1 --magnification=8000 $i || echo "*********** $i FAILED *************"
     mv $i.pfb pfb/
   done
 }
-gen roman0 \
+gen roman0csc0 \
   omsltt10 \
+  omtcsc10 \
   omtt10 \
   omtt12 \
   omtt8 \
@@ -33,6 +32,9 @@ gen roman2 \
   ombx8 \
   ombx9 \
   ombxsl10 \
+  omdunh10 \
+  omff10 \
+  omfib8 \
   omr10 \
   omr12 \
   omr17 \
@@ -57,11 +59,13 @@ gen roman2 \
   omssi8 \
   omssi9 \
   omssq8 \
-  omssqi8
+  omssqi8 \
+  omvtt10
 gen textit0 \
   omitt10
 gen textit2 \
   ombxti10 \
+  omfi10 \
   omti10 \
   omti12 \
   omti7 \
